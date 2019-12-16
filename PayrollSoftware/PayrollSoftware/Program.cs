f5600626d334ffd11a7ac3f6154b68d49c900824
Epiczd <nutball37@gmail.com>
2019-12-16 10:46:07 -0500

    Working on PaySlip Class

--- PayrollSoftware/PayrollSoftware/Program.cs
+++ PayrollSoftware/PayrollSoftware/Program.cs
@@ -187,7 +187,68 @@ namespace PayrollSoftware
 
     class PaySlip
     {
+        private int month;
+        private int year;
 
+        enum MonthsOfYear { JAN = 1, FEB = 2, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC}
+        public PaySlip(int payMonth, int payYear)
+        {
+            month = payMonth;
+            year = payYear;
+
+        }
+
+        public void GeneratePaySlip(List<Staff> myStaff)
+        {
+            string path;
+
+            foreach(Staff f in myStaff)
+            {
+                path = f.NameOfStaff + ".txt";
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("PAYSLIP FOR {0} {1}", (MonthsOfYear)month, year);
+                    sw.WriteLine("===================");
+                    sw.WriteLine("Name of Staff: {0}", f.NameOfStaff);
+                    sw.WriteLine("Hours Worked: {0}", f.HoursWorked);
+                    sw.WriteLine(" ");
+                    sw.WriteLine("Basic Pay: {0:C}", f.BasicPay);
+                    if(f.GetType() == typeof(Manager))
+                    {
+                        sw.WriteLine("Allowance: {0:C}", ((Manager)f).Allowance);
+                    }
+                    else if(f.GetType() == typeof(Admin))
+                    {
+                        sw.WriteLine("Overtime: {0:C}", ((Admin)f).Overtime);
+                    }
+
+                    sw.WriteLine(" ");
+                    sw.WriteLine("===================");
+
+                    sw.WriteLine("Total Pay: {0:C}", f.TotalPay);
+                    sw.WriteLine("===================");
+
+                    sw.Close();
+                }
+            }
+        }
+
+        public void GenerateSummary(List<Staff> myStaff)
+        {
+            var result
+                = from f in myStaff
+                  where f.HoursWorked < 10
+                  orderby f.NameOfStaff ascending
+                  select new { f.NameOfStaff, f.HoursWorked };
+
+            string path = "/Users/21Millisorz/Documents/GitHub/Payroll-Software/summary.txt";
+
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+
+            }
+        }
     }
 
     class Program
