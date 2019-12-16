1cabb39babad59519ac3b9f473909a12ea40a06b
Epiczd <nutball37@gmail.com>
2019-12-16 10:13:24 -0500

    Completed the FileReader class

--- PayrollSoftware/PayrollSoftware/Program.cs
+++ PayrollSoftware/PayrollSoftware/Program.cs
@@ -136,8 +136,41 @@ namespace PayrollSoftware
 
     class FileReader
     {
+        public List<Staff> ReadFile()
+        {
+            List<Staff> myStaff = new List<Staff>;
+            string[] result = new string[2];
+            string path = "/Users/21Millisorz/Documents/GitHub/Payroll-Software/staff.txt";
+            string seperator = ", ";
 
+            if (File.Exists(path))
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        result = sr.ReadLine().Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+
+                        if(result[1] == "Manager")
+                        {
+                            myStaff.Add(new Manager(result[0]));
+                        }
+                        else if(result[1] == "Admin")
+                        {
+                            myStaff.Add(new Admin(result[0]));
+                        }
+                        sr.Close();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: File does not exist");
+            }
+            return myStaff;
+        }
     }
+
 
     class PaySlip
     {
